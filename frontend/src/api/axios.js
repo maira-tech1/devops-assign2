4c6027cb308e4d27f3cc6a9ad5526541e47da818
Maira <mairamalyk13@gmail.com>
2026-04-11 02:42:10 +0500

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,12 +1,9 @@
 import axios from 'axios';
-
-const BASE_URL = 'import.meta.env.VITE_API_URL';
-
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api/v1';
 export default axios.create({
     baseURL: BASE_URL,
-    withCredentials: true  // ← ADD THIS
+    withCredentials: true
 });
-
 export const axiosPrivate = axios.create({
     baseURL: BASE_URL,
     headers: { 'Content-Type': 'multipart/form-data' },
